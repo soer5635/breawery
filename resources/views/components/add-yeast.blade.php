f768b608530bb0d9198b56a835de2f5d88556c22
soer5635 <soeren@andersen-tonder.dk>
2024-10-24 17:31:00 +0200

    Fixed bug related to description field not being submitted

--- resources/views/components/add-yeast.blade.php
+++ resources/views/components/add-yeast.blade.php
@@ -43,9 +43,7 @@
     <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600  mt-4">
         <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800 ">
             <label for="comment" class="sr-only">Your comment</label>
-            <textarea id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400 " placeholder="Write a comment..."></textarea>
+            <textarea id="comment" name="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400 " placeholder="Write a comment..."></textarea>
         </div>
     </div>
-
-
 </div>
